0a3a7065537eb6dc3579c88e9f7ceb2038891da0
liujian1211 <2903426070@qq.com>
2024-03-12 14:21:59 +0800

    perspective project

--- flask_deploy.py
+++ flask_deploy.py
@@ -1,13 +1,16 @@
 import os
-from flask import Flask, request
+from flask import Flask, request,send_file
 import numpy as np
 import cv2
-from PIL import Image
+from PIL import Image,ImageDraw
 import requests
 import json
 import  sqlite3
 import pickle
 import ast
+from io import BytesIO
+import base64
+import urllib.parse
 
 app = Flask(__name__)
 
@@ -24,12 +27,55 @@ def upload_image():
     else:
         return '没有上传成功'
 
+#角点检测
+def corners_det(image,points,corners_quality,euclidean_dist):
+    # corners_quality 和 euclidean_dist的数据类型为str
+
+    image_cv = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+    mask = np.zeros(image_cv.shape[:2], dtype=np.uint8)
+    cv2.fillPoly(mask, [points], (255))
+
+    # 在原图上根据掩膜图像抠出多边形区域
+    image_crop = cv2.bitwise_and(image_cv, image_cv, mask=mask)
+
+    #二值化的图
+    _,binary = cv2.threshold(image_crop,127,255,cv2.THRESH_BINARY)
+
+    #将二值化的图转为灰度图
+    gray_image = cv2.cvtColor(binary,cv2.COLOR_BGR2GRAY)
+
+    # 使用Good Features to Track方法检测角点
+    corners = cv2.goodFeaturesToTrack(gray_image, 4, float(corners_quality), float(euclidean_dist))  
+    #后期把0.1改为corners_quality，20改为euclidean_dist
+
+    #凸包算法筛选出最外面的4个点
+    hull = cv2.convexHull(corners)
+
+    # 绘制凸包
+    for point in hull:
+        x, y = point[0]
+        cv2.circle(image_cv, (int(x), int(y)), 5, (0,0,255), -1) 
+
+    cv2.polylines(image_cv,[points],isClosed=True,color=(0,255,0),thickness=2)
+
+    # 将图像从BGR转换为RGB
+    image_rgb = cv2.cvtColor(image_cv, cv2.COLOR_BGR2RGB)
+
+    # 将图像转换为PIL格式
+    image_pil = Image.fromarray(image_rgb)
+
+    return image_pil,hull
+
 @app.route('/Get_Image_Name', methods=['GET'])
 def get_image_name():
     car_number = request.args.get('car_number')  #获取车牌号
     device_number = request.args.get('device_number')  #获取设备号
     image_name = request.args.get('image_name')  #获取第一张图，此时返回的是url
-    p1 = request.args.get('p1')  #p1:'321.72,241.92'
+
+    corners_quality = request.args.get("corners_quality") #滑动条的形式获取角点质量，值越大，点越少，反之越多
+    euclidean_dist = request.args.get("euclidean_dist") #滑动条的形式获取欧式距离,值越大，点越疏，反之越密
+
+    p1 = request.args.get('p1')  
     p2 = request.args.get('p2')
     p3 = request.args.get('p3')
     p4 = request.args.get('p4')
@@ -46,133 +92,71 @@ def get_image_name():
     p4_x = float(p4.split(',')[0])
     p4_y = float(p4.split(',')[1])
 
-    square_size = 20  # 可以控制缩放比例
-
-    if image_name:
-        # print(f'p1的值为{p1}')
-        # print(f'p2的值为{p2}')
-        # print(f'p3的值为{p3}')
-        # print(f'p4的值为{p4}')
+    if image_name: #image_name为网络图片格式，即https://devtdc.iquanzhan.com/himsFile/upload/flvcut/flv_video_13515277111_221.png
 
         response = requests.get(image_name, stream=True)
         response.raise_for_status()
         image = Image.open(response.raw)
-        # image.save('./saved_images/first_img.jpg')
-        # img1 = cv2.imread('./saved_images/first_img.jpg')
 
-        #**************改为手动添加点2023-11-15**************
-        src_points = np.float32([
-            [p1_x, p1_y],
-            [p2_x, p2_y],
-            [p3_x, p3_y],
-            [p4_x, p4_y]
-        ]) #从左上角为起点开始顺时针
+        points = np.array([(p1_x/3.75,p1_y/3.6),(p2_x/3.75,p2_y/3.6),(p3_x/3.75,p3_y/3.6),(p4_x/3.75,p4_y/3.6)],np.int32)
+        image_lines,corners = corners_det(image=image,points=points,corners_quality=corners_quality,euclidean_dist=euclidean_dist)  #角点检测
+        # image_lines.save("./saved_images/test_lines.jpg")
+        
+        #将绘制了线段的图像转换为字节流
+        image_byte = BytesIO() 
+        image_lines.save(image_byte,format='JPEG') 
+        
+        # #将图像字节流编码为Base64字符串，以便后期传给json
+        image_base64 = base64.b64encode(image_byte.getvalue()).decode('utf-8')
 
-        dst_points = np.float32([
-                [0, 0],  #左上角
-                [440, 0], #右上角
-                [440, 240], #右下角
-                [0, 240] #左下角
-            ])
-        #单位：cm,2张棋盘格叠加
+        # 将字符串形式的图像数据转换为URL格式
+        image_url = 'data:image/jpeg;base64,' + image_base64
 
+        src_points = corners
+
+        if src_points.shape[0] == 4:
+            dst_points = np.float32([
+                    [0, 0],  #左上角
+                    [440, 0], #右上角
+                    [440, 240], #右下角
+                    [0, 240] #左下角
+                ])
+            #单位：cm,2张棋盘格叠加
+
+            # ************************************** M保存在数据库中 **************************************
+            # 连接到SQLite数据库
+            conn = sqlite3.connect('database.db')
+
+            #创建一个游标对象
+            cursor = conn.cursor()
+
+            M = cv2.getPerspectiveTransform(src_points, dst_points)
+
+            #将M转为字符串格式
+            m_str = np.array2string(M, separator=',')
+
+            #创建表数据表（如果不存在）：表名M，列名car_number , m_value
+            cursor.execute("CREATE TABLE IF NOT EXISTS M (car_number, m_value)")
+
+            # 插入透视变换矩阵M的值到表中,根据车牌号将各自的m_str插入到表中。
+            cursor.execute("INSERT INTO M (car_number,m_value) VALUES (?,?)", (car_number,m_str))
+
+            # 保存更改
+            conn.commit()
+
+            # 关闭数据库连接
+            conn.close()
         # ************************************** M保存在数据库中 **************************************
-        # 连接到SQLite数据库
-        conn = sqlite3.connect('database.db')
+            # np.savetxt('./saved_M/' + car_number + '.txt', M) #舍弃掉原来的保存在本地的做法
+            json_data = {"errorCode": 0, "errorMessage": "生成校准矩阵完毕","content":image_url}
+            json_str = json.dumps(json_data)
+            print('校准完毕')
+            return json_str
+        else:
+            json_data = {"errorCode": 1, "errorMessage": "检测到不足4个点","content":image_url}
+            json_str = json.dumps(json_data)
+            return json_str
 
-        #创建一个游标对象
-        cursor = conn.cursor()
-
-        M = cv2.getPerspectiveTransform(src_points, dst_points)
-        # warped_image = cv2.warpPerspective(img1, M, (square_size * (16 - 2), square_size * (12 - 2)))
-        # cv2.imwrite('./saved_images/img1_warped.jpg',warped_image)
-
-        #将M转为字符串格式
-        m_str = np.array2string(M, separator=',')
-
-        #创建表数据表（如果不存在）：表名M，列名car_number , m_value
-        cursor.execute("CREATE TABLE IF NOT EXISTS M (car_number, m_value)")
-
-        # 插入透视变换矩阵M的值到表中,根据车牌号将各自的m_str插入到表中。
-        cursor.execute("INSERT INTO M (car_number,m_value) VALUES (?,?)", (car_number,m_str))
-
-        # 保存更改
-        conn.commit()
-
-        # 关闭数据库连接
-        conn.close()
-        # ************************************** M保存在数据库中 **************************************
-
-        # np.savetxt('./saved_M/' + car_number + '.txt', M) #舍弃掉原来的保存在本地的做法
-        json_data = {"errorCode": 0, "errorMessage": "生成校准矩阵完毕"}
-        json_str = json.dumps(json_data)
-        print('校准完毕')
-        return json_str
-        # **************改为手动添加点2023-11-15**************
-
-        # response = requests.get(image_name, stream=True)
-        # response.raise_for_status()
-        # image = Image.open(response.raw)
-        # image.save('./saved_images/first_img.jpg')
-        #
-        # #若采用网络读取
-        # img1 = cv2.imread('./saved_images/first_img.jpg')
-        #
-        # # 定义棋盘格的行数和列数
-        # rows = 16
-        # cols = 16
-        #
-        # # 棋盘格的交叉点数目
-        # num_corners = (cols - 1) * (rows - 1)
-        #
-        # # 设置棋盘格方块的大小
-        # square_size = 20  # 可以控制缩放比例
-        #
-        # # 计算棋盘格的交叉点坐标
-        # obj_points = np.zeros((num_corners, 3), np.float32)
-        # obj_points[:, :2] = np.mgrid[0:cols - 1, 0:rows - 1].T.reshape(-1, 2) * square_size
-        #
-        # # 转换为灰度图像
-        # gray = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
-        #
-        # ret, corners = cv2.findChessboardCorners(gray, (cols - 1, rows - 1), None)
-        #
-        # if ret:
-        #     # 亚像素级角点检测
-        #     corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1),
-        #                                 criteria=(cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001))
-        #
-        #     # 绘制角点
-        #     cv2.drawChessboardCorners(img1, (cols - 1, rows - 1), corners2, ret)
-        #
-        #     # cv2.imwrite('./img1_corners.jpg',img1)
-        #
-        #     # 定义棋盘格的目标点和图像点
-        #     src_points = np.float32([corners2[0], corners2[cols - 2], corners2[corners2.shape[0] - cols + 1],
-        #                              corners2[corners2.shape[0] - 1]])
-        #
-        #     dst_points = np.float32([
-        #         [0, 140],
-        #         [0, 0],
-        #         [140, 140],
-        #         [140, 0]
-        #     ])
-        #
-        #     # 计算透视变换矩阵
-        #     M = cv2.getPerspectiveTransform(src_points, dst_points)
-        #     warped_image = cv2.warpPerspective(img1, M, (square_size * (cols - 2), square_size * (rows - 2)))
-        #     # cv2.imwrite('./img1_warped.jpg',warped_image)
-        #     np.savetxt('./saved_M/'+car_number+'.txt',M)
-        #     json_data = {"errorCode": 0, "errorMessage": "找到棋盘格图片"}
-        #     json_str = json.dumps(json_data)
-        #     print('校准完毕')
-        #     return json_str
-        # else:
-        #     json_data = {"errorCode": 1, "errorMessage": "未找到棋盘格图片"}
-        #     json_str = json.dumps(json_data)
-        #     print('未找到棋盘格')
-        #     return json_str
-        #     # return '未找到棋盘格'
     else:
         json_data = {"errorCode": 1, "errorMessage": "上传第一张图片失败"}
         json_str = json.dumps(json_data)
@@ -223,7 +207,10 @@ def correct_result():
     test_image_name = request.args.get('test_image_name')
     car_number = request.args.get('car_number')
 
-    correct_p1 = request.args.get('correct_p1')  # p1:'321.72,241.92'
+    corners_quality = request.args.get("corners_quality") #滑动条的形式获取角点质量，值越大，点越少，反之越多
+    euclidean_dist = request.args.get("euclidean_dist") #滑动条的形式获取欧式距离,值越大，点越疏，反之越密
+
+    correct_p1 = request.args.get('correct_p1')  
     correct_p2 = request.args.get('correct_p2')
     correct_p3 = request.args.get('correct_p3')
     correct_p4 = request.args.get('correct_p4')
@@ -262,6 +249,11 @@ def correct_result():
     # ************************************** 在数据库中根据车牌号提取出M **************************************
 
     if test_image_name:
+
+        response = requests.get(test_image_name, stream=True)
+        response.raise_for_status()
+        image = Image.open(response.raw)
+
         correct_p1_x = float(correct_p1.split(',')[0])
         correct_p1_y = float(correct_p1.split(',')[1])
 
@@ -274,90 +266,51 @@ def correct_result():
         correct_p4_x = float(correct_p4.split(',')[0])
         correct_p4_y = float(correct_p4.split(',')[1])
 
-        # 得到变换前的4个角点坐标
-        src_p1 = (correct_p1_x, correct_p1_y)
-        src_p2 = (correct_p2_x, correct_p2_y)
-        src_p3 = (correct_p3_x, correct_p3_y)
-        src_p4 = (correct_p4_x, correct_p4_y)
-        ret_p1, ret_p2, ret_p3, ret_p4 = transformed_cor(M, src_p1, src_p2, src_p3, src_p4)
-        print(f'棋盘角点变换前的4个顶点坐标为：{src_p1},{src_p2},{src_p3},{src_p4}')
-        print(f'棋盘角点变换后的4个顶点坐标为：{ret_p1},{ret_p2},{ret_p3},{ret_p4}')
+        points = np.array([(correct_p1_x/3.75,correct_p1_y/3.6),(correct_p2_x/3.75,correct_p2_y/3.6),
+                           (correct_p3_x/3.75,correct_p3_y/3.6),(correct_p4_x/3.75,correct_p4_y/3.6)],np.int32)
+        image_lines,corners = corners_det(image=image,points=points,corners_quality=corners_quality,euclidean_dist=euclidean_dist)  #角点检测
 
-        area_corners = cal_area(ret_p1,ret_p2,ret_p3,ret_p4) #计算像素面积
-        true_area_corners = round(area_corners/scale,2) #计算实际面积，保留2位小数
-        print(f'棋盘角点的像素面积为{area_corners}')
-        print(f'棋盘角点的实际面积为{str(round(area_corners/scale,2))}')
-        json_data = {"content": true_area_corners, "errorCode": 0, "errorMessage": '校验成功'}
-        json_str = json.dumps(json_data)
-        return json_str
+        #将绘制了线段的图像转换为字节流
+        image_byte = BytesIO() 
+        image_lines.save(image_byte,format='JPEG') 
+        
+        # #将图像字节流编码为Base64字符串，以便后期传给json
+        image_base64 = base64.b64encode(image_byte.getvalue()).decode('utf-8')
 
-        # response = requests.get(test_image_name, stream=True)
-        # response.raise_for_status()
-        # image = Image.open(response.raw)
-        # image.save('./saved_images/second_img.jpg')
-        #
-        # # 若采用网络地址读取
-        # img2 = cv2.imread('./saved_images/second_img.jpg')
+        # 将字符串形式的图像数据转换为URL格式
+        image_url = 'data:image/jpeg;base64,' + image_base64
+        
+        if corners.shape[0] == 4:
+            src_p1 = tuple(corners[0][0])
+            src_p2 = tuple(corners[1][0])
+            src_p3 = tuple(corners[2][0])
+            src_p4 = tuple(corners[3][0])
 
+            print(f'棋盘角点变换前的4个顶点坐标为:{src_p1},{src_p2},{src_p3},{src_p4}')
 
-        # # 定义棋盘格的行数和列数
-        # rows = 16
-        # cols = 16
-        #
-        # # 棋盘格的交叉点数目
-        # num_corners = (cols - 1) * (rows - 1)
-        #
-        # # 设置棋盘格方块的大小
-        # square_size = 20  # 可以控制缩放比例
-        # # warped_image = cv2.warpPerspective(img2, M, (square_size * (cols - 2), square_size * (rows - 2)))
-        # # cv2.imwrite('./img2_warped.jpg',warped_image)
-        #
-        # # 计算棋盘格的交叉点坐标
-        # obj_points = np.zeros((num_corners, 3), np.float32)
-        # obj_points[:, :2] = np.mgrid[0:cols - 1, 0:rows - 1].T.reshape(-1, 2) * square_size
-        #
-        # # 转换为灰度图像
-        # gray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
-        #
-        # ret, corners = cv2.findChessboardCorners(gray, (cols - 1, rows - 1), None)
-        # if ret:
-        #     # 亚像素级角点检测
-        #     corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1),
-        #                                 criteria=(cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001))
-        #
-        #     # 绘制角点
-        #     cv2.drawChessboardCorners(img2, (cols - 1, rows - 1), corners2, ret)
-        #
-        #     # cv2.imwrite('./img2_corners.jpg',img2)
-        #
-        #     # 定义棋盘格的目标点和图像点
-        #     src_points = np.float32([corners2[0], corners2[cols - 2], corners2[corners2.shape[0] - cols + 1],
-        #                              corners2[corners2.shape[0] - 1]])
-        #
-        #     coordinates = src_points[:,0,:]
-        #     tuple_coordinates = tuple(map(tuple,coordinates.tolist()))
-        #     #得到变换前的4个角点坐标
-        #     src_p1 = tuple_coordinates[0]
-        #     src_p2 = tuple_coordinates[1]
-        #     src_p3 = tuple_coordinates[2]
-        #     src_p4 = tuple_coordinates[3]
-        #     ret_p1, ret_p2, ret_p3, ret_p4 = transformed_cor(M, src_p1, src_p2, src_p3, src_p4)
-        #     print(f'棋盘角点变换前的4个顶点坐标为：{src_p1},{src_p2},{src_p3},{src_p4}')
-        #     print(f'棋盘角点变换后的4个顶点坐标为：{ret_p1},{ret_p2},{ret_p3},{ret_p4}')
-        #
-        #     area_corners = cal_area(ret_p1,ret_p2,ret_p3,ret_p4) #计算像素面积
-        #     true_area_corners = round(area_corners/scale,2) #计算实际面积
-        #     print(f'棋盘角点的像素面积为{area_corners}')
-        #     print(f'棋盘角点的实际面积为{str(round(area_corners/scale,2))}')
-        #     json_data = {"content": true_area_corners, "errorCode": 0, "errorMessage": '校验成功'}
-        #     json_str = json.dumps(json_data)
-        #     return json_str
-        #
-        # else:
-        #     json_data = {"errorCode": 1, "errorMessage": "未找到第二张图片的棋盘格"}
-        #     json_str = json.dumps(json_data)
-        #     print('未找到第二张图片的棋盘格，请重新上传第二张测试图')
-        #     return json_str
+            # 得到变换前的4个角点坐标
+            # src_p1 = (correct_p1_x, correct_p1_y)
+            # src_p2 = (correct_p2_x, correct_p2_y)
+            # src_p3 = (correct_p3_x, correct_p3_y)
+            # src_p4 = (correct_p4_x, correct_p4_y)
+            ret_p1, ret_p2, ret_p3, ret_p4 = transformed_cor(M, src_p1, src_p2, src_p3, src_p4)
+            
+            print(f'棋盘角点变换后的4个顶点坐标为:{ret_p1},{ret_p2},{ret_p3},{ret_p4}')
+
+            area_corners = cal_area(ret_p1,ret_p2,ret_p3,ret_p4) #计算像素面积
+            true_area_corners = round(area_corners/scale,2) #计算实际面积，保留2位小数
+            print(f'棋盘角点的实际面积为{str(round(area_corners/scale,2))}')
+
+            json_data_child = {"true_area_corners":true_area_corners, "image_url":image_url}
+            json_data = {"content": json_data_child, "errorCode": 0, "errorMessage": '校验成功'}
+            json_str = json.dumps(json_data)
+            return json_str
+        else:
+            json_data = {"content":image_url,"errorCode": 1, "errorMessage": "角点不足四个点"}
+            json_str = json.dumps(json_data)
+            print('角点不足四个点')
+            return json_str
+
     else:
         json_data = {"errorCode": 1, "errorMessage": "第二个图上传失败"}
         json_str = json.dumps(json_data)
@@ -365,4 +318,4 @@ def correct_result():
         return json_str
 
 if __name__ == '__main__':
-    app.run('10.0.1.34', 8888, debug=False)  # 本机IP
+    app.run('10.0.1.120', 8888, debug=False)  # 本机IP
--- test.py
+++ test.py
@@ -1,174 +1,43 @@
-import os
-<<<<<<< HEAD
-import ast
-=======
-
->>>>>>> ce0564bf044b5106cd8982755bd248a3b7a341e6
-import numpy as np
+from PIL import Image, ImageDraw
 import cv2
-import time
-import json
-import math
+import numpy as np
 
-scale = 100
+# 打开图像
+image = Image.open('/home/ps/honghesong/test_perspective/saved_images/test_lines.jpg')
 
+image_cv = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
 
-def get_image_name():
-    square_size = 20
-    img1 = cv2.imread('D:/test_perspective/test_videos_pics/correct.jpg')
-    #**************改为手动添加点2023-11-15**************
-    src_points = np.float32([
-        [893, 826],
-        [1267, 842],
-        [1320, 887],
-        [842, 863]
-    ]) #从左上角为起点开始顺时针
+# 定义四个点的坐标
+points = np.array([(50, 50), (350, 50), (350, 200), (50, 200)], np.int32)
 
+mask = np.zeros(image_cv.shape[:2], dtype=np.uint8)
+cv2.fillPoly(mask, [points], (255))
 
-    # dst_points = np.float32([
-    #         [0, 0],  #左上角
-    #         [320, 0], #右上角
-    #         [320, 240], #右下角
-    #         [0, 240] #左下角
-    #     ])
+# 在原图上根据掩膜图像抠出多边形区域
+image_crop = cv2.bitwise_and(image_cv, image_cv, mask=mask)
 
-    dst_points = np.float32([
-        [893, 826],  # 变换后的左上角，应该与原图的左上角一致
-        [893+320, 826],  # 右上角
-        [893+320, 826+240],  # 右下角
-        [893, 826+240]  # 左下角
-    ])
-    #单位：cm,4张棋盘格叠加
+#二值化的图
+_,binary = cv2.threshold(image_crop,127,255,cv2.THRESH_BINARY)
 
-    M = cv2.getPerspectiveTransform(src_points, dst_points)
-    # warped_image = cv2.warpPerspective(img1, M, (square_size * (32 - 2), square_size * (24 - 2)))
-    warped_image = cv2.warpPerspective(img1, M, (1920,1080))
-    cv2.imwrite('./saved_images/img1_warped.jpg',warped_image)
-    np.savetxt('./saved_M/'  + 'test.txt', M)
-    json_data = {"errorCode": 0, "errorMessage": "生成校准矩阵完毕"}
-    json_str = json.dumps(json_data)
-    print('校准完毕')
-    return json_str
+#将二值化的图转为灰度图
+gray_image = cv2.cvtColor(binary,cv2.COLOR_BGR2GRAY)
 
-def transformed_cor(M, p1, p2, p3, p4):
-    p1 = p1 + (1,)
-    p2 = p2 + (1,)
-    p3 = p3 + (1,)
-    p4 = p4 + (1,)
+# 运行Harris角点检测算法
+# corners = cv2.cornerHarris(gray_image, blockSize=2, ksize=3, k=0.04)
+# corners = cv2.dilate(corners, None)  # 对角点进行膨胀操作，增加角点的突出性
 
-    ret1 = np.dot(M, p1)
-    ret2 = np.dot(M, p2)
-    ret3 = np.dot(M, p3)
-    ret4 = np.dot(M, p4)
+corners = cv2.goodFeaturesToTrack(gray_image, 100, 0.01, 10)  # 使用Good Features to Track方法检测角点
 
-    p1_x = round(ret1[0] / ret1[2], 2)
-    p1_y = round(ret1[1] / ret1[2], 2)
+#凸包算法筛选出最外面的4个点
+hull = cv2.convexHull(corners)
+# 绘制凸包
+for point in hull:
+    x, y = point[0]
+    cv2.circle(image_cv, (int(x), int(y)), 5, (0,0,255), -1)
 
-    p2_x = round(ret2[0] / ret2[2], 2)
-    p2_y = round(ret2[1] / ret2[2], 2)
+# 绘制角点
+# threshold = 0.1  # 设定角点的阈值
+# image_with_corners = cv2.cvtColor(gray_image, cv2.COLOR_GRAY2BGR)
+# image_with_corners[corners > threshold * corners.max()] = [0, 0, 255]  # 标记角点的位置为红色
 
-    p3_x = round(ret3[0] / ret3[2], 2)
-    p3_y = round(ret3[1] / ret3[2], 2)
-
-    p4_x = round(ret4[0] / ret4[2], 2)
-    p4_y = round(ret4[1] / ret4[2], 2)
-
-    point1 = ()
-    point2 = ()
-    point3 = ()
-    point4 = ()
-
-    point1 = point1 + (p1_x, p1_y)
-    point2 = point2 + (p2_x, p2_y)
-    point3 = point3 + (p3_x, p3_y)
-    point4 = point4 + (p4_x, p4_y)
-    return point1, point2, point3, point4,
-
-def cal_area(p1,p2,p3,p4):
-    area1 = abs(0.5 * ( (p1[0]*p2[1]) + (p2[0]*p4[1]) + (p4[0]*p1[1]) - (p2[0]*p1[1]) - (p4[0]*p2[1]) - (p1[1]*p4[1])))
-    area2 = abs(0.5 * ( (p2[0]*p3[1]) + (p3[0]*p4[1]) + (p4[0]*p2[1]) - (p3[0]*p2[1]) - (p4[0]*p3[1]) - (p2[0]*p4[1])))
-    return round(area1+area2,2)
-
-def cal_ret_area(p1,p2,p3,p4):
-    w = min(abs(p2[0]-p1[0]),abs(p3[0]-p4[0]))
-    h = min(abs(p3[1]-p2[1]),abs(p4[1]-p1[1]))
-    #p1和p2中y值最大的点，p3和p4中y值最小的点，组成的矩形作为面积
-    # lt = p1 if p1[1]>p2[1] else p2
-    # rb = p3 if p3[1]<p4[1] else p4
-    # w = abs(rb[0] - lt[0])
-    # h = abs(rb[1] - lt[1])
-    scale_area = 1 - abs(w / h - 1)  # 该公式用来表示与1的接近程度，w/h越接近1，scale越大，即视线越近，面积应该乘以的系数就越大
-    print(f'scale_area为{scale_area}')
-    print(f'变换后的w为{w},h为{h}')
-    return round(w * h * scale_area, 2)
-
-def correct_result():
-    # correct_p1 = request.args.get('correct_p1')  # p1:'321.72,241.92'
-    # correct_p2 = request.args.get('correct_p2')
-    # correct_p3 = request.args.get('correct_p3')
-    # correct_p4 = request.args.get('correct_p4')
-    #********ret3井盖坐标********
-    correct_p1 = '900, 860'
-    correct_p2 = '1037, 860'
-    correct_p3 = '1037,889'
-    correct_p4 = '900,889'
-    # ********ret3井盖坐标********
-
-    M = np.loadtxt('./saved_M/' +'test.txt')
-    print(f'M的值为{M}')
-
-    square_size=20
-    img1 = cv2.imread('D:/test_perspective/crop_imgs/larger_test.jpg')
-    # warped_image = cv2.warpPerspective(img1, M, (square_size * (32 - 2), square_size * (24 - 2)))
-    warped_image = cv2.warpPerspective(img1, M, (1920,1080))
-
-    correct_p1_x = float(correct_p1.split(',')[0])
-    correct_p1_y = float(correct_p1.split(',')[1])
-
-    correct_p2_x = float(correct_p2.split(',')[0])
-    correct_p2_y = float(correct_p2.split(',')[1])
-
-    correct_p3_x = float(correct_p3.split(',')[0])
-    correct_p3_y = float(correct_p3.split(',')[1])
-
-    correct_p4_x = float(correct_p4.split(',')[0])
-    correct_p4_y = float(correct_p4.split(',')[1])
-
-    # 得到变换前的4个角点坐标
-    src_p1 = (correct_p1_x, correct_p1_y)
-    src_p2 = (correct_p2_x, correct_p2_y)
-    src_p3 = (correct_p3_x, correct_p3_y)
-    src_p4 = (correct_p4_x, correct_p4_y)
-    ret_p1, ret_p2, ret_p3, ret_p4 = transformed_cor(M, src_p1, src_p2, src_p3, src_p4)
-
-    cv2.circle(warped_image,(int(ret_p1[0]),int(ret_p1[1])),5,(0,255,0),-1)
-    cv2.circle(warped_image, (int(ret_p2[0]),int(ret_p2[1])), 5, (0, 255, 0), -1)
-    cv2.circle(warped_image, (int(ret_p3[0]),int(ret_p3[1])), 5, (0, 255, 0), -1)
-    cv2.circle(warped_image, (int(ret_p4[0]),int(ret_p4[1])), 5, (0, 255, 0), -1)
-    cv2.imwrite('./saved_images/manhole3_warped.jpg', warped_image)
-
-    print(f'棋盘角点变换前的4个顶点坐标为：{src_p1},{src_p2},{src_p3},{src_p4}')
-    print(f'棋盘角点变换后的4个顶点坐标为：{ret_p1},{ret_p2},{ret_p3},{ret_p4}')
-
-    # area_corners = cal_area(ret_p1,ret_p2,ret_p3,ret_p4) #计算像素面积
-    area_corners = cal_ret_area(ret_p1,ret_p2,ret_p3,ret_p4)
-    true_area_corners = round(area_corners,2) #计算实际面积，保留2位小数
-    print(f'棋盘角点的像素面积为{area_corners}')
-    print(f'棋盘角点的实际面积为{true_area_corners}平方厘米')
-    json_data = {"content": true_area_corners, "errorCode": 0, "errorMessage": '校验成功'}
-    json_str = json.dumps(json_data)
-    return json_str
-
-if __name__ == '__main__':
-    # get_image_name()
-<<<<<<< HEAD
-    # correct_result()
-    m_str = "[[ 1.20771149e-01, -3.20408340e-01, 4.70644005e+01], [-9.09599780e-03, -1.61812770e-01, 8.00876169e+01], [-1.44767448e-04, -1.38279140e-03, 1.00000000e+00]]"
-    m_list = ast.literal_eval(m_str)
-    M = np.array(m_list)
-
-    # 打印转换后的NumPy数组
-    print(M)
-=======
-    correct_result()
->>>>>>> ce0564bf044b5106cd8982755bd248a3b7a341e6
+cv2.imwrite('./saved_images/hull.jpg',image_cv)
